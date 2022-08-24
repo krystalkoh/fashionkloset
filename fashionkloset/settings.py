6646039469a7826edd4e05dcab34993e0d850ca3
Krystal <krystalkoh@gmail.com>
2022-08-24 22:03:47 +0800

    updated cloudinary settings

--- fashionkloset/settings.py
+++ fashionkloset/settings.py
@@ -32,11 +32,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 #Cloudinary config
-cloudinary.config(
-  cloud_name = config('CLOUD_NAME'),
-  api_key = config('API_KEY'),
-  api_secret = config('API_SECRET'),
-)
+cloudinary = {
+    'CLOUD_NAME' : config('CLOUD_NAME'),
+    'API_KEY' : config('API_KEY'),
+    'API_SECRET' : config('API_SECRET'),
+}
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
