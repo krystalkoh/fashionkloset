278e3f72413448c3ce1ac7a15bb2a301ec108ea9
Krystal <krystalkoh@gmail.com>
2022-08-25 02:47:32 +0800

    cmon

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -45,9 +45,7 @@ const Upload = () => {
 
   useEffect(() => {
     console.log(`component is mounted`);
-
     const token = authService.getCurrentUser().access;
-
     const decoded = jwt_decode(token);
     console.log(decoded.user_email);
     setEmail(decoded.user_email);
@@ -60,13 +58,10 @@ const Upload = () => {
     data.append("cloud_name", "krystalk");
 
     try {
-      fetch(
-        "https://api.cloudinary.com/v1_1/krystalk/image/upload",
-        {
-          method: "POST",
-          body: data,
-        }
-      );
+      fetch("https://api.cloudinary.com/v1_1/krystalk/image/upload", {
+        method: "POST",
+        body: data,
+      });
       const imageUrlData = res.json();
       console.log(imageUrlData);
       setImageUrl(imageUrlData.url);
@@ -75,42 +70,42 @@ const Upload = () => {
       console.log(err + "error with image upload");
     }
   };
-  
+  const handleform = async () => {
+    let form_data = new FormData();
+    imageUrl && form_data.append("imageUrl", imageUrl);
+    form_data.append("image", image);
+    form_data.append("name_of_item", name_of_item);
+    form_data.append("description", description);
+    form_data.append("price", price);
+    form_data.append("brand", brand);
+    form_data.append("size", size);
+    form_data.append("email", email);
+    form_data.append("tags", tags);
+    console.log(form_data);
+
+    try {
+      const url = "/api/clothes/posts/";
+      const res = await fetch(url, {
+        headers: {
+          Authorization: "Bearer " + authService.getCurrentUser().access,
+        },
+        method: "POST",
+        body: form_data,
+      });
+
+      const products = await res.json();
+      console.log(products);
+    } catch (error) {
+      console.log("error.message with the other form data");
+    }
+    alert("You have added an item to sell!");
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     uploadImage();
-    
-      let form_data = new FormData();
-      imageUrl && form_data.append("imageUrl", imageUrl);
-      form_data.append("image", image);
-      form_data.append("name_of_item", name_of_item);
-      form_data.append("description", description);
-      form_data.append("price", price);
-      form_data.append("brand", brand);
-      form_data.append("size", size);
-      form_data.append("email", email);
-      form_data.append("tags", tags);
-      console.log(form_data);
-
-      try {
-        const url = "/api/clothes/posts/";
-        const res = await fetch(url, {
-          headers: {
-            Authorization: "Bearer " + authService.getCurrentUser().access,
-          },
-          method: "POST",
-          body: form_data,
-        });
-
-        const products = await res.json();
-        console.log(products);
-      } catch (error) {
-        console.log("error.message with the other form data");
-      }
-      alert("You have added an item to sell!");
-    
+    imageUrl && handleform();
   };
-
   // const handleSubmit = async (e) => {
   //   e.preventDefault();
   // const url = "https://api.cloudinary.com/v1_1/krystalk/image/upload";
